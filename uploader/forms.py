3af4ebcd3c7087c82d7974f8b36a1a5e3774a20d
mirsalsa <mirsasalsa@gmail.com>
2022-06-05 02:19:38 +0700

    remove unused code

--- uploader/forms.py
+++ uploader/forms.py
@@ -1,16 +1,7 @@
-from tkinter import Widget
 from django import forms
 from .models import Files
 
 class FilesForm(forms.ModelForm):
-    # file_name = forms.CharField(
-    #     widget = forms.TextInput(
-    #         attrs={
-    #             'class':'input-height',
-    #         }
-    #     )
-    # )
-    # file_docs = forms.FileField()
     class Meta:
       model = Files
       fields = ['file_name', 'file_docs']

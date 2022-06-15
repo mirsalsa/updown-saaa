51ec9e1912bf3ad7ef5b3c3af15eafe49e102f30
mirsalsa <mirsasalsa@gmail.com>
2022-06-15 13:01:34 +0700

    add day and unixtime for file

--- uploader/models.py
+++ uploader/models.py
@@ -1,13 +1,19 @@
+from datetime import date
 import os
 from django.db import models
+from unixtimestampfield.fields import UnixTimeStampField
 
 # Create your models here.
 def create_name(instance, filename):
   path = 'documents/'
   filename, fileextensions = os.path.splitext(filename)
-  return os.path.join(path, f"{filename}{fileextensions}")
+  day = instance.file_date.strftime("%A").upper()
+  tanggal = instance.file_date.timestamp()
+  second, milisecond = str(tanggal).split(".")
+  file_name = "%s-%s" % (day, second)
+  return os.path.join(path, f"{file_name}{fileextensions}")
 
 class Files(models.Model):
     file_name = models.CharField(max_length=100)
-    file_docs = models.FileField(upload_to=create_name)
     file_date = models.DateTimeField(auto_now_add=True)
+    file_docs = models.FileField(upload_to=create_name)
